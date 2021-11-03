59a19869972cd9810720a9db42525c4723454fe3
Vadim Kuznetsov <90134447+VK00323@users.noreply.github.com>
2021-11-03 23:31:59 +0500

    Добавлена база данных

---
+++ app/src/main/java/com/example/rickandmortyrxjava3/database/CharacterDao.kt
@@ -0,0 +1,17 @@
+package com.example.rickandmortyrxjava3.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.rickandmortyrxjava3.pojo.PojoResult
+
+@Dao
+interface CharacterDao {
+    @Query("SELECT * FROM character_table")
+    fun getAllCharacter(): LiveData<List<PojoResult>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllCharacter(list : List<PojoResult>)
+}
--- app/src/main/java/com/example/rickandmortyrxjava3/pojo/PojoResult.kt
+++ app/src/main/java/com/example/rickandmortyrxjava3/pojo/PojoResult.kt
@@ -1,9 +1,10 @@
 package com.example.rickandmortyrxjava3.pojo
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-
+@Entity(tableName = "character_table")
 data class PojoResult(
 @PrimaryKey
 @SerializedName("id") @Expose
